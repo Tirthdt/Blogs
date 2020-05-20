1cb74c3336280674fd6b553248ea6f4900b518c0
Tirth Trivedi <31176677+Tirthdt@users.noreply.github.com>
2020-05-20 20:03:11 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyB-aMQrZGIG0TRS7fH4D_AbluPj4Q65f1c",
-    authDomain: "sample-b0ffe.firebaseapp.com",
-    databaseURL: "https://sample-b0ffe.firebaseio.com",
-    projectId: "sample-b0ffe",
-    storageBucket: "sample-b0ffe.appspot.com",
-    messagingSenderId: "100404234294",
-    appId: "1:100404234294:web:57a0c1511b973922fc7097",
-  },
+
+
+    databaseURL: "https://sample-b0
+
+
+
+
+
 };
 
 /*
